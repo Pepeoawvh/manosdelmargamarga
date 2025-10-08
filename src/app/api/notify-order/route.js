953ed06f18504cad8cb3418c240a2ce1b165bc5b
Pepeoawvh <jpp.valdes@gmail.com>
2025-10-08 12:05:08 -0300

    resend fix

--- src/app/api/notify-order/route.js
+++ src/app/api/notify-order/route.js
@@ -3,9 +3,8 @@ export const runtime = "nodejs";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 import admin, { adminDb } from "../../../lib/firebase/admin";
-// Ojo con el path/case exacto según tu proyecto:
-import { CustomerReceipt } from "../../components/emails/CustomerReceipt";
-import { AdminNotice } from "../../components/emails/AdminNoticee";
+import { CustomerReceipt } from "../../components/emails/customerReceipt";
+import { AdminNotice } from "../../components/emails/AdminNotice";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
