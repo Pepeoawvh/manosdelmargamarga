7da203026019d9b0458eb1e54f47cd68dcaa9e3f
Pepeoawvh <jpp.valdes@gmail.com>
2025-12-12 14:37:41 -0300

    Actualización de seguridad Next/React y migración a Turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  turbopack: {},
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**', // Puedes restringirlo si quieres más seguridad
+        hostname: '**',
       },
-    ],
-    domains: [
-      'i.pinimg.com',
-      'pinimg.com',
-      'media.pinterest.com',
-      'www.pinterest.com',
-      's.pinimg.com',
-      'drive.google.com',
-      'lh3.googleusercontent.com',
     ],
     formats: ['image/avif', 'image/webp'],
   },
+
   experimental: {
     optimizeCss: true,
     scrollRestoration: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = { fs: false };
-    }
-    return config;
   },
 };
 
