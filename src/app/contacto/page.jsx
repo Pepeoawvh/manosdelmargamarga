80ff08037f5ba14dc93a2925a7e2d55edf9084ab
Pepe Valdes <jpp.valdes@gmail.com>
2026-07-26 13:32:22 -0400

    fix: usar fill en imagen contacto para que object-top funcione + limpieza código

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -1,31 +1,27 @@
 "use client";
-import React from "react";
+
 import FormularioContacto from "../components/contact/FormularioContacto";
 import Image from "next/image";
+
 export default function ContactoPage() {
   return (
     <main className="min-h-screen bg-[#f5f3e6]">
       <section className="px-4 md:px-6 py-10">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-          {/* Columna izquierda: frase e ilustración */}
           <div className="space-y-8">
-
-            <div className="w-full aspect-[4/3]  grid place-items-center">
+            <div className="relative w-full aspect-[4/3] overflow-hidden rounded">
               <Image
                 src="/images/contacto/contactoimg.svg"
                 alt="Ilustración de contacto Manos del Marga Marga"
-                width={1000}
-                height={1000}
+                fill
                 className="object-cover object-top"
+                sizes="(max-width: 1024px) 100vw, 50vw"
               />
             </div>
           </div>
 
-          {/* Columna derecha: datos de contacto + formulario */}
           <div>
-
-            <div className="grid  gap-4 mb-4 text-sm">
-
+            <div className="grid gap-4 mb-4 text-sm">
               <div className="bg-white border border-gray-100 text-center rounded-xl p-4 shadow-sm">
                 <p className="font-medium text-gray-700">Horario de atención</p>
                 <p className="text-gray-500">Lunes a Viernes de 10:00 a 17:00 hrs.</p>
@@ -33,7 +29,7 @@ export default function ContactoPage() {
               </div>
             </div>
 
-            <h3 className="text-2xl text-center font-semibold text-[#3f4f1c] ">
+            <h3 className="text-2xl text-center font-semibold text-[#3f4f1c]">
               Déjanos tu mensaje
             </h3>
             <div className="bg-white rounded-xl p-6 shadow-sm border border-gray-100 mt-4">
